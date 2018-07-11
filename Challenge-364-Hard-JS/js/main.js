74f84a3a20d360b2ddb316407e8af49feed6a7c4
kevin wilkinson <willkillson@gmail.com>
2018-07-10 22:07:13 -0700

    added project Challenge-364-Hard-js

---
+++ Challenge-364-Hard-JS/js/main.js
@@ -0,0 +1,25 @@
+﻿var canvas = document.querySelector('canvas');
+var scoreboard = document.getElementById("score");
+
+canvas.width = 500;
+canvas.height = 500;
+
+
+var c = canvas.getContext('2d');
+
+
+
+
+function mainloop() {
+
+
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.beginPath();
+    c.fillStyle = 'black';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+ 
+
+    requestAnimationFrame(mainloop);
+}
+
+requestAnimationFrame(mainloop);

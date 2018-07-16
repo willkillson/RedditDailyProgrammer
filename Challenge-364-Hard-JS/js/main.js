5148065d219f25103a96a6b35212c650c0c7e065
Wilkinson, Kevin Mitchell - (willkillson) <willkillson@gmail.com>
2018-07-15 23:27:11 -0700

    printPiece

--- Challenge-364-Hard-JS/js/main.js
+++ Challenge-364-Hard-JS/js/main.js
@@ -1,15 +1,26 @@
-﻿//var canvas = document.querySelector('canvas');
-//var scoreboard = document.getElementById("score");
+﻿
 
-//canvas.width = 500;
-//canvas.height = 500;
-//var c = canvas.getContext('2d');
 
-////vars
-//var boardSizeWidth = 5;
-//var boardSizeHeight = 5;
+//vars
+var boardSizeWidth = 5;
+var boardSizeHeight = 5;
+
+const F = 'F';
+const I = 'I';
+const L = 'L';
+const N = 'N';
+const P = 'P';
+const T = 'T';
+const U = 'U';
+const V = 'V';
+const W = 'W';
+const X = 'X';
+const Y = 'Y';
+const Z = 'Z';
 
 //endvar
+
+
 function createBoard(sizex, sizey) {
     var board = new Array(sizex * sizey);
     //init board to all spaces
@@ -20,34 +31,49 @@ function createBoard(sizex, sizey) {
     return board;
 }
 
-//const F = 'F';
-//const I = 'I';
-//const L = 'L';
-//const N = 'N';
-//const P = 'P';
-//const T = 'T';
-//const U = 'U';
-//const V = 'V';
-//const W = 'W';
-//const X = 'X';
-//const Y = 'Y';
-//const Z = 'Z';
+function createPiece(type) {
+    var width = 3;//max width
+    var height = 5;//max height
+
+    var piece = Array(width * height);
+    for (let i = 0; i < piece.length; i++) {
+        piece[i] = ' ';
+    }
+
+    switch (type) {
+        case I:
+            piece[0] = I;
+            piece[3] = I;
+            piece[6] = I;
+            piece[9] = I;
+            piece[12] = I;
+            break;
+    }
+
+    return piece;
+}
+
+function printPiece(charArray) {
+
+    console.log(charArray[0] + charArray[1] + charArray[2]);
+    console.log(charArray[3] + charArray[4] + charArray[5]);
+    console.log(charArray[6] + charArray[7] + charArray[8]);
+    console.log(charArray[9] + charArray[10] + charArray[11]);
+    console.log(charArray[12] + charArray[13] + charArray[14]);
+
+}
+
+let p = createPiece(I);
+
+printPiece(p);
 
 
+//var canvas = document.querySelector('canvas');
+//var scoreboard = document.getElementById("score");
 
-
-
-
-//function addNumbers(a, b) {
-//    return a + b;
-//}
-
-
-//var k = 5;
-//var l = 3;
-
-//var p = addNumbers(k, l);
-//console.log(p);
+//canvas.width = 500;
+//canvas.height = 500;
+//var c = canvas.getContext('2d');
 
 //function mainloop() {
 
